f2c054c21bac8f6c4b62d614da673c01d15203af
hanuman226 <hanuman22698@gmail.com>
2021-08-27 15:32:28 +0530

    working on exam screen

--- src/Components/ExamScreen.js
+++ src/Components/ExamScreen.js
@@ -1,5 +1,6 @@
 import { Col, Form, Row } from "react-bootstrap";
 import styled from "styled-components";
+import CountDownTimer from "./exams/CountDownTimer";
 
 export default function ExamScreen() {
     return (
@@ -13,7 +14,7 @@ export default function ExamScreen() {
             </Header>
             <Body>
                 <p className="text-dark">The Narasimhan Committee for Financial sector Reforms has suggested reduction in?</p>
-                <hr/>
+                <HorizontalBreak/>
                 <fieldset>
     <Form.Group as={Row} className="mb-3">
       <Col sm={10}>
@@ -45,9 +46,18 @@ export default function ExamScreen() {
     </Form.Group>
   </fieldset>
             </Body>
-            <Footer>footer</Footer>
+            <Footer>
+              <div>
+              <Button bgColor="hsl(300deg 100% 25%)"  bgHoverColor="hsl(300deg 100% 35%)">Mark for Review & Next</Button>
+              <Button bgColor="hsl(0deg 100% 40%)" bgHoverColor="hsl(0deg 100% 50%)">Clear Response</Button>
+              <Button bgColor="hsl(39deg 100% 40%)" bgHoverColor="hsl(39deg 100% 50%)">Bookmark</Button>
+              </div>
+              <Button bgColor="hsl(120deg 100% 25%)" bgHoverColor="hsl(120deg 100% 35%)">Save & Next</Button>
+              </Footer>
          </LeftPanel>
-         <RightPanel>right</RightPanel>
+         <RightPanel>
+           <CountDownTimer/>
+         </RightPanel>
      </Wrapper>
     );
 }
@@ -55,15 +65,20 @@ export default function ExamScreen() {
 
 const Wrapper =styled.div`
 display:grid;
-grid-template-columns:1fr 20rem;
+grid-template-columns:1fr 25rem;
 margin:0 auto;
 height:100%;
 width:100%;
 box-shadow: 0 0 1.875rem 0.3125rem hsl(0deg 0% 0% / 20%);
 @media (max-width:991px){
-    grid-template-columns:1fr 20rem;
+    grid-template-columns:1fr;
 }
 `;
+
+const HorizontalBreak=styled.hr`
+height:1px;
+background-color: silver;
+`
 
 const Text=styled.p`
 margin-bottom: 0;
@@ -72,6 +87,29 @@ font-weight: bold;
 & span {
     color:${props=>props.color?props.color:"white"}
 }
+`
+
+const Button = styled.button`
+    height: 2.5rem;
+    /* width: 20rem; */
+    /* border-radius: 0.625rem; */
+    border: none;
+    text-transform: uppercase;
+    padding: 0.625rem; 
+    font-size: 0.75rem;
+    font-weight: bold;
+    background-color: ${props=>props.bgColor?props.bgColor:"black"};
+    color: white;
+    cursor: pointer;
+    /* box-shadow:${props=>props.disabled?"none": "0 0 0.3125rem 0.125rem hsl(0deg 0% 0% / 40%)"}; */
+    outline: none;
+  transition:all 0.2s;
+  &:hover{
+    background-color: ${props=>props.bgHoverColor?props.bgHoverColor:"black"};
+  }
+  /* &:active {
+    box-shadow: none;
+  } */
 `
 
 const LeftPanel= styled.div`
@@ -106,6 +144,17 @@ padding:1rem;
 `
 const Footer= styled.div`
 margin-top:auto;
-height: 3rem;
-background-color: pink;
+height: 4rem;
+padding:0 1rem;
+display:flex;
+justify-content:space-between;
+align-items: center;
+background-color: black;
+
+& div button{
+  margin:0 1rem;
+}
+& div:first-child{
+  margin-left:0;
+}
 `
--- src/actions/examActions.js
+++ src/actions/examActions.js
@@ -1,38 +1,43 @@
-import Axios from "axios";
+import axios from "axios";
 import {
   FREE_EXAM_LIST_FAIL,
   FREE_EXAM_LIST_REQUEST,
   FREE_EXAM_LIST_SUCCESS,
 } from "../constants/examContants";
 
+const api = axios.create({
+  baseURL: `https://www.errortechnologies.com/quizdemo/apis`,
+});
 const freeExamList = () => async (dispatch, getState) => {
   dispatch({ type: FREE_EXAM_LIST_REQUEST });
   try {
     const {
       userSignin: { userInfo },
     } = getState();
-    const { data } = await Axios.post(
-      "https://www.errortechnologies.com/quizdemo/apis/test_free_exam",
-      {},
+    const { data } = await api.post(
+      "/test_free_exam",{},
       {
-        headers: {
-          tokenu: userInfo.Token,
-          Id: userInfo.Id,
-          server_key: "3w99V63pW7tJ7vavGXtCKo8cp",
+
+        'headers': {
+          'tokenu': userInfo.Token,
+          'Id': userInfo.Id,
+          'server_key': "3w99V63pW7tJ7vavGXtCKo8cp",
         },
-      }
+    }
     );
-    // const data = await fetch("https://www.errortechnologies.com/quizdemo/apis/test_free_exam",{
-    //   method: "POST",
-    //   mode:"cors",
-    //   data:'',
-    //   headers:{
-    //     'tokenu': userInfo.Token,
-    //     'Id': userInfo.Id,
-    //     'server_key': '3w99V63pW7tJ7vavGXtCKo8cp',
+    // const {data} = await axios(
+    //   {
+    //     url:"https://www.errortechnologies.com/quizdemo/apis/test_free_exam",
+    //     method: "post",
+    //     headers: {
+    //       tokenu: userInfo.Token,
+    //       Id: userInfo.Id,
+    //       server_key: "3w99V63pW7tJ7vavGXtCKo8cp",
+    //     },
     //   }
-    // }).then(res=>res.json());
-    // console.log(data);
+    // );
+
+    console.log(data);
     dispatch({ type: FREE_EXAM_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: FREE_EXAM_LIST_FAIL, payload: error.message });
