9b41b42c8efff3dec78b9573174814aa3b970e1a
hanuman226 <hanuman22698@gmail.com>
2021-08-28 20:31:21 +0530

    modified neltify.toml

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,25 +1,22 @@
 import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FancyButton from "./FancyButton";
- export default function CustomModal(props) {
-   const {children,title,show,toggle,link}=props;
+export default function CustomModal(props) {
+  const { children, title, show, toggle, link } = props;
   return (
-      <Modal show={show} onHide={toggle}   backdrop="static"
-      keyboard={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{children}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={toggle}>
-            Close
-          </Button>
-          <Link to={link}>
-          <FancyButton onClick={toggle} >
-           start now
-          </FancyButton>
-          </Link>
-        </Modal.Footer>
-      </Modal>
+    <Modal show={show} onHide={toggle} backdrop="static" keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={toggle}>
+          Close
+        </Button>
+        <Link to={link}>
+          <FancyButton onClick={toggle}>start now</FancyButton>
+        </Link>
+      </Modal.Footer>
+    </Modal>
   );
 }
